7f6d11776b2b617c3cc5d10b0b876be391f2ab48
Alitzel Lopez Sanchez <lopa3401@dinf-aevo-03i.DInf.fsci.usherbrooke.ca>
2026-06-23 11:49:40 -0400

    feat(operators): Added split genome operator.

--- src/pangesim/reconstruction/operators.py
+++ src/pangesim/reconstruction/operators.py
@@ -2,6 +2,7 @@
 
 import random as rd
 from itertools import combinations
+from itertools import pairwise
 from typing import List
 from typing import Tuple
 
@@ -14,7 +15,7 @@ class MergeOperator(RefinementOperator):
     """Merges two genomes."""
 
     def __init__(self, random: bool = True, genomes: List[Genome] | None = None):
-        """Constructor of the sorting strategy.
+        """Constructor of the merging operator.
 
         Args:
            random: Choose two genomes randomly.
@@ -84,5 +85,78 @@ class MergeOperator(RefinementOperator):
 
                 pangenome.add_genome(new_genome)
                 return True
+
+        return False
+
+
+class SplitOperator(RefinementOperator):
+    """Splits a genome."""
+
+    def __init__(self, random: bool = True, genome: Genome | None = None):
+        """Constructor of the splitting operator.
+
+        Args:
+           random: Choose one genome randomly.
+           genome: If not random, provide a specific genome.
+        """
+        self.random = random
+        if genome is None:
+            self.genome = None
+        else:
+            self.genome = genome
+
+    def split(self, core: set, k: int) -> Tuple[Genome, Genome] | None:
+        """Splits a given genome according to core.
+
+        Args:
+           core: The core set of the pangenome
+           k: the number of genomes in the given pangenome.
+
+        Returns:
+           Two genomes if split is possible, None otherwise.
+        """
+        if self.genome.path_count <= 1:
+            return None
+        else:
+            genome_a = Genome(genome_id=self.genome._genome_id)
+            genome_b = Genome(genome_id=k)
+            paths = self.genome.get_path_sequences()
+
+            for i, path in enumerate(paths):
+                path_genes = set(path)
+                if path_genes & core:
+                    for edge in pairwise(path):
+                        genome_a.add_edge(edge)
+                        genome_b.add_edge(edge)
+                else:
+                    if i % 2 == 0:
+                        for edge in pairwise(path):
+                            genome_a.add_edge(edge)
+                    else:
+                        for edge in pairwise(path):
+                            genome_b.add_edge(edge)
+            return (genome_a, genome_b)
+
+    def improve(self, pangenome: Pangenome) -> bool:
+        """Splits a random or given genome.
+
+        Args:
+           pangenome: Input pangenome
+
+        Returns:
+           True if merging is possible, merges genomes on the fly.
+        """
+        if len(pangenome) == 0:
+            return False
+
+        if self.random:
+            self.genome = rd.choice(pangenome._genomes)
+
+        pair = self.split(pangenome.core, len(pangenome))
+
+        if pair:
+            pangenome.replace_genome(self.genome._genome_id, pair[0])
+            pangenome.add_genome(pair[1])
+            return True
 
         return False
